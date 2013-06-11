170b1c7364795eafcd9196f3078eb2447dde873f
chaseoes <chase.oes@gmail.com>
2013-06-11 15:05:39 -0700

    Attempted fix.

--- src/main/java/com/chaseoes/servermenu/ServerMenu.java
+++ src/main/java/com/chaseoes/servermenu/ServerMenu.java
@@ -14,52 +14,52 @@ import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
 public class ServerMenu extends JavaPlugin implements Listener {
-	
+
 	IconMenu menu;
-	
+
 	public void onEnable() {
 		saveDefaultConfig();
 		getServer().getPluginManager().registerEvents(this, this);
 		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 		menu = new IconMenu(ChatColor.translateAlternateColorCodes('&', getConfig().getString("title")), roundUp(getConfig().getConfigurationSection("servers").getKeys(false).size()), new IconMenu.OptionClickEventHandler() {
-            @Override
-            public void onOptionClick(IconMenu.OptionClickEvent event) {
-                warpToServer(event.getPlayer(), getConfig().getString("servers." + event.getPosition() + ".server"));
-                event.setWillClose(true);
-            }
-        }, this);
+			@Override
+			public void onOptionClick(IconMenu.OptionClickEvent event) {
+				warpToServer(event.getPlayer(), getConfig().getString("servers." + event.getPosition() + 1 + ".server"));
+				event.setWillClose(true);
+			}
+		}, this);
 		for (String s : getConfig().getConfigurationSection("servers").getKeys(false)) {
 			int i = Integer.parseInt(s);
 			menu.setOption(i - 1, new ItemStack(Material.getMaterial(getConfig().getString("servers." + i + ".item")), 1), translate(getConfig().getString("servers." + i + ".name")), translate(getConfig().getString("servers." + i + ".description")));
 		}
 	}
-	
+
 	public void onDisable() {
 		reloadConfig();
 		saveConfig();
 		menu = null;
 	}
-	
+
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
 		if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getPlayer().getItemInHand().getType() == Material.getMaterial(getConfig().getString("item"))) {
 			menu.open(event.getPlayer());
 		}
 	}
-	
+
 	private void warpToServer(Player player, String server) {
-	    ByteArrayDataOutput out = ByteStreams.newDataOutput();
-	    out.writeUTF("Connect");
-	    out.writeUTF(server);
-	    player.sendPluginMessage(this, "BungeeCord", out.toByteArray());
+		ByteArrayDataOutput out = ByteStreams.newDataOutput();
+		out.writeUTF("Connect");
+		out.writeUTF(server);
+		player.sendPluginMessage(this, "BungeeCord", out.toByteArray());
 	}
-	
+
 	private String translate(String s) {
 		return ChatColor.translateAlternateColorCodes('&', ChatColor.RESET + ChatColor.stripColor(s));
 	}
-	
+
 	int roundUp(int n) {
-	    return (n + 8) / 9 * 9;
+		return (n + 8) / 9 * 9;
 	}
 
 }
